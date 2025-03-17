3086687591d471e972ce8c43a705e5ccc586a365
Bill Newman <william.newman@seattlecolleges.edu>
2025-03-17 15:03:40 -0700

    Adds initial Calculator code

---
+++ src/main/java/com/github/newmanix/calculator_wn25_2/Main.java
@@ -0,0 +1,11 @@
+package com.github.newmanix.calculator_wn25_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
